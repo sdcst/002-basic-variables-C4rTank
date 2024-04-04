b690ab296a681547b498593a947838cc5f873ced
C4rTank <hassamsdss@gmail.com>
2024-04-04 12:50:41 -0700

    Basic Variables

---
+++ assignment2.py
@@ -0,0 +1,17 @@
+#! python3
+
+# SD Computing Studies Assignment
+
+"""
+
+Create a file called assignment2.py\
+Create variables fname and lname\
+Assign the string "Mr" to fname and the string "Yang" to lname\
+Output is not necessary.  The contents of the variables will be checked from within the program.\
+
+"""
+
+fname = "Mr"
+lname = "Yang"
+
+print(fname, lname)
---
+++ assignment3.py
@@ -0,0 +1,15 @@
+#! python3
+
+# SD Computing Studies Assignment
+
+"""
+
+Create a file called assignment3.py\
+Create variables value1 and value2\
+Assign the numerical value of 10 to value1 and the string "10" to value2\
+Output is not necessary.  The contents of the variables will be checked from within the program.\
+
+"""
+
+value1 = 10
+value2 = "10"
